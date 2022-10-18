c0bb48c66db07df8110d512f01e08daf6628cb6e
yashdondal <yashdondal21@gmail.com>
2022-10-18 19:13:40 +0530

    Find Max No

---
+++ FindMaxNo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTestMax
+{
+    public class FindMaxNo
+    {
+        public void MaximumNum(int firstval, int Secondval, int thirdval)
+        {
+            if (firstval.CompareTo(Secondval) > 0 && firstval.CompareTo(thirdval) > 0 ||
+               firstval.CompareTo(Secondval) >= 0 && firstval.CompareTo(thirdval) > 0 ||
+               firstval.CompareTo(Secondval) > 0 && firstval.CompareTo(thirdval) >= 0)
+            {
+                Console.WriteLine("first value is greatest");
+            }
+            if (Secondval.CompareTo(firstval) > 0 && Secondval.CompareTo(thirdval) > 0 ||
+               Secondval.CompareTo(firstval) >= 0 && Secondval.CompareTo(thirdval) > 0 ||
+               Secondval.CompareTo(firstval) > 0 && Secondval.CompareTo(thirdval) >= 0)
+            {
+                Console.WriteLine("Second value is greatest");
+            }
+            if (thirdval.CompareTo(firstval) > 0 && thirdval.CompareTo(Secondval) > 0 ||
+               thirdval.CompareTo(firstval) >= 0 && thirdval.CompareTo(Secondval) > 0 ||
+               thirdval.CompareTo(firstval) > 0 && thirdval.CompareTo(Secondval) >= 0)
+            {
+                Console.WriteLine("third value is greatest");
+            }
+
+
+        }
+
+    }
+}
