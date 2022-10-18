a2e79b6e6decaf011277ac22d2fbf24e054fb60f
yashdondal <yashdondal21@gmail.com>
2022-10-18 19:12:05 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericFindMax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaxNo findMaxNo = new FindMaxNo();
+            findMaxNo.MaximumNum(10, 20, 30);
+
+            FindMaxString findMaxstring = new FindMaxString();
+            findMaxstring.MaximumString("yash", "yashuuu", "yashuuuuu");
+
+            FindMaxFloat findMaxfloat = new FindMaxFloat();
+            findMaxfloat.MaximumFloat(21.3, 4.5, 6.7);
+        }
+    }
+}
